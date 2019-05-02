741e5f003f979054d78db7c2d2daeaed99e2529c
Corey Noble <coreynoble@hotmail.ca>
2019-05-01 20:42:56 -0400

    React Weather App - Button Syntax Fix

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ const Form = props => (
     <form onSubmit={props.getWeather}>
         <input type="text" name="city" placeholder="City..."/>
         <input type="text" name="country" placeholder="Country..."/>
-        <buton>Get Weather</buton>
+        <button>Get Weather</button>
     </form>
 );
 
