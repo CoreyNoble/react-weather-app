633c8112ea5673b4d1955730dcd4789decebb771
Corey Noble <coreynoble@hotmail.ca>
2019-05-11 20:55:36 -0400

    Adding Font Awesome Icons, Main containers using flexbox

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const Form = props => (
     <form onSubmit={props.getWeather}>
-        <input type="text" name="city" placeholder="City..."/>
-        <input type="text" name="country" placeholder="Country..."/>
-        <button>Get Weather</button>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8">
+                <input type="text" name="city" placeholder="City..."/>
+                <input type="text" name="country" placeholder="Country..."/>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <button>Get Weather</button>
+            </div>
+        </div>
     </form>
 );
 
